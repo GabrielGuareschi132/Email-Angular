98531a87d1686be8fba30f5e63df0f2c43b42d29
Joao Gabriel Leite Guareschi <gabrielguareschi35@outlook.com>
2024-08-28 09:56:31 -0300

    Subindo Email

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'email';
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      to_name: '',
+      from_name: '',
+      from_email: '',
+      message: '',
+    });
+  }
+
+  async send() {
+    emailjs.init('ipEEfm5fDU1GgnoVa');
+    let response = await emailjs.send('service_ngo4usw', 'template_ce2lfm3', {
+      to_name: this.form.value.to_name,
+      from_name: this.form.value.from_name,
+      from_email: this.form.value.from_email,
+      message: this.form.value.message,
+    });
+
+    alert('Mensagem Enviada com Sucesso');
+    this.form.reset();
+  }
+
+  onSubmit() {
+    throw new Error('Method not implemented.');
+  }
 }
---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [],
+  imports: [BrowserModule, ReactiveFormsModule],
+  providers: [],
+  bootstrap: [],
+})
+export class AppModule {}
